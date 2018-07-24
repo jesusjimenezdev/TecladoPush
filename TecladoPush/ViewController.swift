3a8fa0a643d3ab87e499e7713975b2f9752b30e0
jesusjimenezdev <info@jesusjimenez.net>
2018-07-24 10:13:36 +0200

    final commit

---
+++ TecladoPush/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var texto1: UITextField!
+    @IBOutlet weak var texto2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        texto1.delegate = self
+        texto2.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(teclado(notificacion:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(teclado(notificacion:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(teclado(notificacion:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+        texto2.addTarget(self, action: #selector(cambioTeclado), for: UIControlEvents.touchDown)
+        
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    @IBAction func boton(_ sender: Any) {
+        texto2.resignFirstResponder()
+    }
+    
+    @objc func teclado(notificacion: Notification){
+        
+        guard let tecladoUp = (notificacion.userInfo![UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notificacion.name == Notification.Name.UIKeyboardWillShow {
+            self.view.frame.origin.y = -tecladoUp.height
+        }else{
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        print("ejecutar accion")
+        return true
+    }
+    
+    @objc func cambioTeclado(){
+        print("cambio")
+        texto2.keyboardType = .numberPad
+    }
+    
+}
+
